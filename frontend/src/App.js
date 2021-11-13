c3530c413cfd31660b28da4b0f25ee2f74b3ce3e
Akash <akash.0711a@gmail.com>
2021-11-13 09:10:48 +0530

    new files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import ReactMapGL , { Marker , Popup } from 'react-map-gl';
-import { Room } from '@material-ui/icons'
+import { Room , Star } from '@material-ui/icons'
 
 import './App.css';
 
@@ -8,22 +8,21 @@ import './App.css';
 
 function App() {
     const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
-    latitude: 51.1525,
-    longitude: 14.9689,
+    width: "100vw", 
+    height: "100vh", 
+    latitude: 51.1525, 
+    longitude: 14.9689, 
     zoom: 6})
 
-
   return (
-   
+
     <div className="App">
-      <ReactMapGL
+       <ReactMapGL
       {...viewport}
       mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapStyle = "mapbox://styles/akash-singh/ckvvv92vd4btd14m3srydan0t"
-    >
+       > 
       <Marker 
       latitude={51.1525}
       longitude={14.9689} 
@@ -36,12 +35,28 @@ function App() {
           longitude={14.9689}
           closeButton={false}
           closeOnClick={false}
-          anchor="bottom">
-          <div>You are here</div>
-        </Popup>
-      </ReactMapGL>
+          anchor="bottom"> 
+          <div className = "card">
+            <label>Place</label>
+            <h4 className = "place">Gorlitz</h4>
+            <label>Review</label>
+            <p>Nice Place</p>
+            <label className = "stars">Rating</label>
+            <div>
+              <Star className="star"/>
+              <Star className="star"/>
+              <Star className="star"/>
+              <Star className="star"/>
+            </div>
+            <label>Information : </label>
+            <b className = "username">Akash, </b>
+            <span className = "date">1 hour ago</span>
+            </div> 
+        </Popup> 
+      </ReactMapGL> 
       
     </div>
+
   );
 }
 
